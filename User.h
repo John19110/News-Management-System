bfd09677fdb4dfb117bf07d73a29697a77614174
John19110 <113050863+John19110@users.noreply.github.com>
2024-05-12 15:30:43 +0300

    Update User.h

--- User.h
+++ User.h
@@ -3,8 +3,8 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include<map>
 #include<string>
-//4
 using namespace std;
 class User
 {
@@ -12,9 +12,8 @@ public:
 	int id;
 	string username;
 	string password;
-	int attemptCount = 0;
-	int maxAttempts = 3;
 	vector<string> preferredCategories;
+	
 
 	User();
 	User(int, string, string, vector<string>);
@@ -22,7 +21,6 @@ public:
 	string getUserName();
 	string getPassword();
 	bool CheckLogin(string username, string pass);
-	bool CheckAdmin(string name);
 	int getID();
 	string toString() const;
 
